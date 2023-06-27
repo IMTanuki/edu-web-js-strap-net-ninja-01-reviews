b77636d4eb4dfc8fa12e85e9655e4d319f8f0f0f
Tanuki <imtanuki@gmail.com>
2023-06-26 22:44:05 -0400

    CH XX - final - env vars added

--- src/App.js
+++ src/App.js
@@ -12,16 +12,14 @@ import Home from './pages/Home/Home';
 import ReviewDetails from './pages/ReviewDetails/ReviewDetails';
 import Categories from './pages/Categories/Categories';
 
-
 // setup apollo client
 const client = new ApolloClient({
-    uri: 'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/cljby5owj2v1e01t202leb6ao/master',
+    uri: `${process.env.REACT_APP_URI_HYGRAPH}`,
     headers: {
-        Authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2ODc3NTM0OTYsImF1ZCI6WyJodHRwczovL2FwaS11cy1lYXN0LTEtc2hhcmVkLXVzZWExLTAyLmh5Z3JhcGguY29tL3YyL2NsamJ5NW93ajJ2MWUwMXQyMDJsZWI2YW8vbWFzdGVyIiwibWFuYWdlbWVudC1uZXh0LmdyYXBoY21zLmNvbSJdLCJpc3MiOiJodHRwczovL21hbmFnZW1lbnQuZ3JhcGhjbXMuY29tLyIsInN1YiI6IjEyNDIyZDg4LWQzN2EtNGZlMy04ZmYxLTZjNWY1NWM0YzliOCIsImp0aSI6ImNsamNjdXNqZjMxeGEwMXVlNzd1eDFna2QifQ.VxfkRxiCjPcxC_NNcX7MS7gXZtNeuHC4ghhIpzyNZk4zQwa4ZTfbuZKhP6EJTOGEM1okuIzr5J8VkGZMx0oc6FHE-DYPA7lL3DSGaMOKa7E133wBIDR6wIpRS_cCg63wTQdoDv04xAKYA51dxzhLK8mYXPQp-J4OSbpsA5BJJerZVPTmtoShA8Ark7B0hXUYhBM1z-hP37FFuJfMo4YaHd1aeuGxXNHxSGSS0FtHuICkuK7aohtau-MJagpCBcUw27CjY64b4nBqkGJl87Xm3D05fLBNd0NlkfhaOxahNqBSV-EX0PJrVIjEjxekP0xorAnfPXsRaG4agqTQ4sYUV8uzWrS6mGO2UgrQU-1qrGqYcj5HB8V8eypDEGqhqNxWRHAAlTl8SL1UeX2OctySPt1mV4k1bUfxo0jZci2zkLpcXJnOEyxmn6tSQ_rDXt6Uw_0Ee0omaCoCx0qkCBpyAS5yv04fLguDuIEHlg9AZiaboIVxIqrkkGUkIvE4dLElRRt7DKJDUf6UycBvKFe4u-gUUgvKSeSQmPIF4_LIDXZRpR4xMPBIP3QxQJYMcM-sdsFQvHefBheX_0H2jFDctx8Oca2FTrdXrKsQ8PZQSM24CTrfYb-S60-8xyW73MHKJmZVm_R8OhT1HcQVJs_syZ17M2maHqdl9Z-JQYJCJyo',
+        Authorization: `Bearer ${process.env.REACT_APP_AUTH_KEY_HYGRAPH}`,
     },
     cache: new InMemoryCache(),
 });
-
 
 
 // function
